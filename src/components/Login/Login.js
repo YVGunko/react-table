d5c065e72c063c50093b24766311e91dee550901
“YVGunko“ <yurygunko@gmail.com>
2023-07-25 12:06:01 +0300

    Login works, server unreachable state is tested on login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Login.css';
 import { baseURLogin } from "../http-common/baseURL";
+import api  from "../http-common/http-common";
 
-async function loginUser(credentials) {
+/*async function loginUser(credentials) {
     console.log(`loginUser, credentials=${credentials.username}`);
     return fetch(baseURLogin, {
       method: 'POST',
@@ -14,12 +15,21 @@ async function loginUser(credentials) {
       body: JSON.stringify(credentials)
     })
       .then(data => data.json())
+   }*/
+
+   async function loginUser(credentials) {
+    console.log(`loginUser, credentials=${credentials.username}`);
+    return api('/login', 'POST', credentials, credentials);
    }
 
    export default function Login({ setToken }) {
     console.log(`Login, start`);
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
+    const [error, setError] = useState({
+      error : false,
+      status : ""
+    });
   
     const handleSubmit = async e => {
       e.preventDefault();
@@ -28,27 +38,52 @@ async function loginUser(credentials) {
         username,
         password
       });
-      console.log(`Login, token=${token.id}`);
-      setToken(token);
+
+      if (token) {
+        console.log(`Login, token?=${token?.error}`);
+        if (token.error) {
+          console.log(`Login, token.error=${token?.error}`);
+          setError({
+            error : token?.error,
+            status : token?.status
+          });
+        }else{
+          console.log(`Login, token=${token}`);
+          setToken(token);
+        }
+      }
     }
-  return(
-    <div className="login-wrapper">
-      <h1>Войдите чтобы продолжить...</h1>
-        <form onSubmit={handleSubmit}>
-        <label>
-            <p>Имя пользователя</p>
-            <input type="text" onChange={e => setUserName(e.target.value)} />
-        </label>
-        <label>
-            <p>Пароль</p>
-            <input type="password" onChange={e => setPassword(e.target.value)} />
-        </label>
-        <div>
-            <button type="submit">Войти</button>
+    if (error?.error) {
+      return(
+      <div className="login-wrapper"><h1>Нет доступа к серверу приложения. Попробуйте позже...</h1></div>
+      )
+    } else {
+      return(
+        <div class="p-4">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-5 col-sm-6 col-lg-3 mx-auto">
+              <div class="formContainer">
+                <h2 class="p-2 text-center mb-4 h4" id="formHeading">Вход тут...</h2>
+                <form onSubmit={handleSubmit}>
+                  <div class="form-group mt-3">
+                    <label class="mb-2" for="username">Имя пользователя </label>
+                    <input onChange={e => setUserName(e.target.value)} class="form-control" id="username" name="username" type="text" />
+                  </div>
+                  <div class="form-group mt-3">
+                    <label class="mb-2" for="password">Пароль</label>
+                    <input  onChange={e => setPassword(e.target.value)} type="password" class="form-control" id="password" name="password" />
+                  </div>
+                  <button type="submit" class="btn btn-success btn-lg w-100 mt-4">Войти</button>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-        </form>
-    </div>
-  )
+        </div>
+
+      )
+    }
 }
 
 Login.propTypes = {
--- src/components/http-common/http-common.js
+++ src/components/http-common/http-common.js
@@ -1,12 +1,41 @@
-import axios from "axios";
 import { baseURL } from "./baseURL";
-const api = (credentials) =>axios.create()({ 
-  baseURL: baseURL,
-  headers: {
-    "Content-Type": "application/json",
-    "Authorization": 'Basic ' + btoa(credentials.name+':'+credentials.password)
-  }
-});
+// General api to acces data from web
+export default function api(path,method,credentials,params){
 
-export default api;
-//
+  let options;
+      options = Object.assign({headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          "Authorization": 'Basic ' + btoa(credentials.username+':'+credentials.password)
+      }},{ method: method }, params ? { body: JSON.stringify(params) } : null );
+
+
+      const NO_RESPONSE_CODE = 0;
+
+      return fetch(baseURL+path, options)
+          .then(resp => {
+            if (resp.ok) {
+              console.log(`General api to acces data from web responced Ok= ${resp?.status}`);
+              return resp.json();
+            } else {
+              console.log(`General api to acces data from web responced with code= ${resp?.status}`);
+              if (resp.status === NO_RESPONSE_CODE) {
+                // server unavailable
+                return Promise.reject(new Error('Server unavailable'));
+              } else {
+                // handle other API errors here, implementation
+                // depends on your response format
+                return Promise.reject(new Error(resp?.status));
+              }
+            }
+          })
+          .then( json => json)
+          .catch((error) => {
+            console.log(`General api to acces data from web error= ${error?.message}`);
+            let err = {
+              error : true,
+              status : error?.message
+            }
+            return err;
+          });
+} 
