a79571ee72bcbeefbbc123d06aa31cea60ae6415
“YVGunko“ <yurygunko@gmail.com>
2023-08-10 14:27:47 +0300

    some work on OrderGrid skeleton

--- src/components/Order/OrderCrud.js
+++ src/components/Order/OrderCrud.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
-
-import TokenContext from '../Token/Token';
+import { useState, useContext} from "react";
+import OrderGrid from "./OrderGrid";
 
 const OrderCrud = () => {
-
+    return (
+        <>
+            <OrderGrid></OrderGrid>
+        </>
+    )
 };
 
 export default OrderCrud;
