8d6e4c25b78e48813b1a0860dc4f44f1f6aa259b
“YVGunko“ <yurygunko@gmail.com>
2023-08-11 15:04:14 +0300

    some progress with order-context, useOrder

--- src/components/Order/order-context.js
+++ src/components/Order/order-context.js
@@ -1,5 +1,8 @@
-import React, { useContext, useState, useCallback } from 'react';
+import React, { useContext, useState, useMemo, useEffect } from 'react';
 import { useForm } from "react-hook-form";
+import { DataGrid, } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 import { CustomerContext } from '../Customer/CustomerCrud';
 import useOrder from './useOrder'
@@ -24,22 +27,25 @@ const OrderProvider = (props) => {
             { field: 'details', headerName: 'Содежание...', headerAlign: 'center', width: 120,  },
         ],
         changeNamedProperty: (property, value) => {
-            setOrderInfo({ ...orderInfo, [property]: value });
+            setOrderInfo({ ...orderInfo, [property]: value.orderInfo });
         },
-      fetchOrder: (property, value) => {
-        setOrderInfo({ ...orderInfo, [property]: value });
-        
-      },
     };
-    /*const customerInformation = {
-        id: "",
-        name: "",
-        email: "",
-        phone: "",
-    };*/
+    const fetchedOrdersData = {
+        rows: [],
+        totalItems: 0,
+        totalPages: 0,
+        currentPage: 0,
+
+        setValues: (fetchedData) => {
+            console.log(`setValues ${JSON.stringify(fetchedData)}`);
+            setFetchedData({rows:fetchedData?.orders ? fetchedData.orders : [], 
+                totalItems:fetchedData?.totalItems ? fetchedData.totalItems : 0});
+        }
+    }
+    const [fetchedData, setFetchedData] = useState(fetchedOrdersData);
     const [orderInfo, setOrderInfo] = useState(orderInformation);
     return (
-      <OrderContext.Provider value={orderInfo}>
+      <OrderContext.Provider value={{orderInfo, fetchedData}}>
         {props.children}
       </OrderContext.Provider>
     );
@@ -50,8 +56,9 @@ const OrderProvider = (props) => {
     return (
         <div className="user">
         <h1 className="profile-userName">{props.selectedCustomer?.id}</h1>
-        <p className="profile-fullName">({context.details})</p>
+        <p className="profile-fullName">({context.orderInfo.details})</p>
         <OrdersTopBox />
+        <OrdersGridBox />
       </div>
       
     );
@@ -61,22 +68,63 @@ const OrderProvider = (props) => {
     const context = useContext(OrderContext);
     return (
       <div className="user">
-        <h1 className="profile-userName">{context?.id}</h1>
-        <p className="profile-fullName">({context.details})</p>
+        <h1 className="profile-userName">{context.orderInfo?.id}</h1>
+        <p className="profile-fullName">({context.fetchedData?.totalItems})</p>
       </div>
     );
   };
+  const OrdersGridBox = () => {
+    const context = useContext(OrderContext);
+    const [loading, setLoading] = useState(false);
+    const [paginationModel, setPaginationModel] = React.useState({ page: 0, pageSize: 10, });
+    function onPaginationModelChange (paginationModelL) {
+        console.log(`onPaginationModelChange ${JSON.stringify(paginationModelL)}`);
+        setPaginationModel({page:paginationModelL.page, pageSize: paginationModelL.pageSize});
+      }
 
+    const [rowSelectionModel, setRowSelectionModel] = React.useState([]);
+    const onRowsSelectionHandler = useMemo(() =>(ids) => {
+        const selectedRowsData = ids.map((id) => context.fetchedData.rows.find((row) => row.id === id));
+        console.log(`onRowsSelectionHandler ${selectedRowsData}`);
+        //customerHasChanged(selectedRowsData[0]);
+      }, [context.fetchedData.rows]);
 
+    useEffect(() => {
+        console.log(`order-context useEffect useOrder.fetchedData`);
+        /*const data = useOrder.fetchedData;
+        if (!data) { data = useOrder.fakeData; }*/
+        context.fetchedData.setValues(useOrder.fetchedData ? useOrder.fetchedData : useOrder.fakeData);
+    }, [useOrder.fetchedData]);
+
+    return (
+        <Box sx={{ height: '100%', width: '100%' }}>
+          <Stack direction="row" spacing={1} sx={{ mb: 1 }}>
+            <DataGrid rows={ context.fetchedData.rows}  columns={ context.orderInfo?.columnsForDataGrid } 
+            rowCount={ context.fetchedData?.totalItems }
+            gridPageCountSelector
+            pageSizeOptions={[10]}
+            paginationMode="server"
+            paginationModel={paginationModel}
+            onPaginationModelChange={ onPaginationModelChange }
+    
+            rowSelectionModel={rowSelectionModel}
+            onRowSelectionModelChange={ (ids) => onRowsSelectionHandler (ids) }
+            keepNonExistentRowsSelected
+    
+            autoHeight={true}
+            loading={loading}
+            />
+          </Stack>
+        </Box>
+      );
+    }
 const OrderBox = () => {
     const {selectedCustomer} = useContext(CustomerContext);
-
     
-      useEffect(() => {
-        fetchData();
-      }, [fetchData]);
+    const data = useOrder().fetchedData ;
+    console.log(`OrderBox  useOrder().fetchedData ${JSON.stringify(data)}`);
     return (        
-        <OrderProvider >
+        <OrderProvider fetchedData={data}>
             <Orders selectedCustomer={selectedCustomer}/>
         </OrderProvider>
     )
--- src/components/Order/useOrder.js
+++ src/components/Order/useOrder.js
@@ -1,21 +1,56 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useContext, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import  api  from "../http-common/http-common";
+import TokenContext from '../Token/Token';
+import { CustomerContext } from '../Customer/CustomerCrud';
+const orderURL = `/orders?customerId=`;
 
 export default function useOrder() {
+    const token = useContext(TokenContext);
+    const {selectedCustomer} = useContext(CustomerContext);
+    const [fetchedData, setFetchedData] = useState();
+    let id = selectedCustomer.id ? selectedCustomer.id : 0;
+
     const fetchData = useCallback(async () => {
-    setLoading(true);
-    return api(getTitleUrl(), 'GET', token)
-    .then(data => {
-      setRows(data.customers);
-      setTotalItems(data.totalItems);
-      setTotalPages(data.totalPages);
-      setCurrentPage(data.currentPage);
-      setLoading(false);
+        console.log(`useOrder()  fetchData ${selectedCustomer}`);
+    return api(`${orderURL}${id}`, 'GET', token)
+    .then(fetchedData => {
+        setFetchedData(fetchedData);
     })
     .catch((error) => {
-      console.log(`CustomerGrid fetchData ${JSON.stringify(error)}`);
-      setRows(fakeRows);
-      setLoading(false);
+      console.log(`useOrder()  fetchData ${JSON.stringify(error)}`);
+      return(error);
       })
 
-  }, [getTitleUrl,token]);
+  }, [selectedCustomer,token]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  
+  const fakeData = {
+  "totalItems": 0,
+  "totalPages": 0,
+  "currentPage": 0,
+  "orders": [
+      {
+          "id": "",
+          "comment": "",
+          "details": "...",
+          "customer_id": "",
+          "division_code": "",
+          "division_name": "...",
+          "user_id": "",
+          "user_name": "",
+          "sample": false,
+          "date": ""
+      },
+    ]}
+    
+  return {
+    fetchedData,
+    fakeData
+  }
 }
